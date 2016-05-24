a1ce01cb272ae1e898c2f68a4bd1217caf589e04
Ruyu You <727809166@qq.com>
2016-05-24 19:07:51 +0800

    finish practice.js

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,18 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var sameElements = buildSameELements(collection_a, collection_b);
+  return sameElements;
+}
+
+function buildSameELements(collection_a, collection_b) {
+  var sameElements = [];
+  collection_a.forEach(function (collectElement) {
+    for (var i=0;i<collection_b.length;i++) {
+      if (collectElement === collection_b[i]) {
+        sameElements.push(collection_b[i]);
+      }
+    }
+  });
+  return sameElements;
 }
 
 module.exports = collect_same_elements;
